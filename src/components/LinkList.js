a66422a6255613dabb54790a072b8a774015888f
Alexander Mire <amire51117@gmail.com>
2018-08-27 01:04:54 -0400

    add mutations to app

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,4 +1,4 @@
-import React, { Compoentn } from 'react';
+import React, { Component } from 'react';
 import Link from './Link';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
