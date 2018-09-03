96f0f9c4a21782fecb8fdbbff3cdbc81319f8383
Alexander Mire <amire51117@gmail.com>
2018-09-02 20:15:37 -0400

    fix typo on index.js and add websocket

--- src/index.js
+++ src/index.js
@@ -10,10 +10,34 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
 import { AUTH_TOKEN } from './constants';
 import { setContext } from 'apollo-link-context';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+
 
 const httpLink = createHttpLink({
-	url: 'http://localhost:4000'
+	uri: 'http://localhost:4000'
 });
+
+const wsLink = new WebSocketLink({
+	uri: `ws://localhost:4000`,
+	options: {
+		reconnect: true,
+		connectionParams: {
+			authToken: localStorage.getItem(AUTH_TOKEN),
+		}
+	}
+})
+
+const link = split(
+	({ query }) => {
+		const { kind, operation } = getMainDefinition(query);
+		return kind === 'OperationDefinition' && operation === 'subscription'
+	},
+	wsLink,
+	authLink.concat(httpLink)
+)
 
 const authLink = setContext((_, { headers }) => {
 	const token = localStorage.getItem(AUTH_TOKEN)
@@ -26,7 +50,7 @@ const authLink = setContext((_, { headers }) => {
 })
 
 const client = new ApolloClient({
-	link: authLink.concat(httpLink),
+	link,
 	cache: new InMemoryCache()
 })
 
